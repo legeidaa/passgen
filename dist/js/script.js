cd8384dbbfd94453334bfeff8e7640ac4f73c884
legeidaa <andrey.ligiza@mail.ru>
2021-07-27 13:53:21 +0300

    добавлены проверки на присутствие символов в массивах для правильного выпадения пароля

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,10 +4,11 @@ let numbers = document.querySelector('#numbers'),
 	symbols = document.querySelector('#symbols'),
 	range = document.querySelector('#range'),
 	rangeSpan = document.querySelector('.label-range span'),
-	result = document.querySelector('.result span'),
+	result = document.querySelector('.result input'),
 
 	btnGen = document.querySelector('form div'),
-	btnCopy = document.querySelector('.result button')
+	btnCopy = document.querySelector('.result button');
+
 
 
 rangeSpan.innerText = range.value;
@@ -16,16 +17,21 @@ range.addEventListener("input", function() {
 });
 
 
-let numbersChars = ['1','2','3','4','5','6','7','8','9','0']
-	upperChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	lowerChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+let numbersChars = ['1','2','3','4','5','6','7','8','9','0'],
+	upperChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+	lowerChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
 	symbolsChars = ['!', '@', '#', '$', '%', '?'];
 
 
 function generatePass() {
 	let passArr = [],
-		finalPass = ''
+		finalPass = '';
 
+	//если ни один из чекбоксов не отмечен, отмечаем номера
+	if (document.querySelectorAll('input[type="checkbox"]:not(:checked)').length == 4) {
+		numbers.checked = true;
+	}
 	if (numbers.checked){
 		passArr = passArr.concat(numbersChars);
 	}
@@ -39,23 +45,63 @@ function generatePass() {
 		passArr = passArr.concat(symbolsChars);
 	}
 
-	if (document.querySelectorAll('input[type="checkbox"]:not(:checked)').length == 4) {
-		console.log ('bla')
+	const sortFunc = () => Math.random() - 0.5;
+	const randomInteger = ( min, max ) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+	passArr.sort(sortFunc);
+
+
+	//проверяем каждый элемент строки на содержание хотя бы одного элемента массива
+	const include = function(arr, string) {
+		console.log(string);
+		let stringArr = string.split('');
+		console.log(!arr.some(i => stringArr.includes(i) > 0))
+		return !arr.some(i => stringArr.includes(i) > 0);
+	};
+
+	function finalSorting(){
+		for (let i = 0; i < range.value; i++) {
+			finalPass += passArr[randomInteger(0, passArr.length - 1)];
+		}
+	}
+	finalSorting();
+
+	//если в конечный результат не попадает хотя бы один символ из всех выбранных массивов, перезапускаем функцию
+	if (symbols.checked  && include(symbolsChars, finalPass)) {
+		while (include(symbolsChars, finalPass)) {
+			finalPass = '';
+			finalSorting();
+		}
+	}
+	else if (numbers.checked  && include(numbersChars, finalPass)) {
+		while (include(numbersChars, finalPass)) {
+			finalPass = '';
+			finalSorting();
+		}
+	}
+	else if (upper.checked  && include(upperChars, finalPass)) {
+		while (include(upperChars, finalPass)) {
+			finalPass = '';
+			finalSorting();
+		}
+	}
+	else if (lower.checked  && include(lowerChars, finalPass)) {
+		while (include(lowerChars, finalPass)) {
+			finalPass = '';
+			finalSorting();
+		}
 	}
 
-	const srotFunc = () => Math.random() - 0.5;
-	const randomInteger = ( min, max ) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
 
-	passArr.sort(srotFunc);
-	console.log(passArr)
 
-	for (let i = 0; i < range.value; i++) {
-        finalPass += passArr[randomInteger(0, passArr.length - 1)];
-    }
+	//хз как сделать проверку на все чекбоксы сразу
 
-	console.log(finalPass)
+	result.value = finalPass;
 }
 
 btnGen.addEventListener('click', generatePass);
 
+btnCopy.addEventListener('click', () => {
+	result.select();
+	document.execCommand('copy')
+});
 
